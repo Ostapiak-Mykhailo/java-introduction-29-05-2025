b725635731286b73557a5e7bef516776eeb5e4bd
Ostapiak-Mykhailo <Ostapiakmz@gmail.com>
2025-05-30 16:08:12 +0300

    Add files via upload

---
+++ HW\/hometask1.java
@@ -0,0 +1,15 @@
+package com.hillel.homework.homeworklesson2;
+
+public class hometask1 {
+
+    public static void main(String[] args) {
+
+        long a = 10L;
+        long b = 20L;
+
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+        System.out.println("a =" + b);
+        System.out.println("b =" + a);
+    }
+}
---
+++ HW\/hometask2.java
@@ -0,0 +1,13 @@
+package com.hillel.homework.homeworklesson2;
+
+public class hometask2 {
+
+    public static void main(String[] args) {
+
+        double a = 48.5522;
+        double b = 24.4238;
+        System.out.println("Івано-Франківська ратуша");
+        System.out.println(a + " - північна широта");
+        System.out.println(b + " - східна довгота");
+    }
+}
